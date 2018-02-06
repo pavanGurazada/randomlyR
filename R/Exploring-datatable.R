4c9aae2fbaec397bff53c102afda4e4fc74d7321
pavanGurazada <pavankumar.gurazada@gmail.com>
2018-02-06 10:25:23 +0400

    Initialized scriot for data.table exploration

---
+++ R/Exploring-datatable.R
@@ -0,0 +1,8 @@
+#' ---
+#' title: "Exploring data.table "
+#' author: Pavan Gurazada
+#' date: ""
+#' output: github_document
+#' ---
+#' 
+#' Exploration of data.table as an alternative to dplyr. 
--- R/anRpackage-internal.R
+++ R/anRpackage-internal.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(data.table)
 
 ggplot(data.frame(x = 1:10,
                   y = 1:10), aes(x, y)) +
